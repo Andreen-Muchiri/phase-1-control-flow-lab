06615c72eac063d5f13e99bfccbf693ef376377a
Andreen <andreenshii@gmail.com>
2022-08-16 01:59:35 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+   if (someValue> 2500) {
+    result = 'No can do.';
+  }
+  else if (someValue > 2000) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+ 
+ else if (someValue <= 400) {
+    result = 'This one is on me!';
+  }
+ 
+  return result
 }
 
-function ternaryCheckCity(){
   // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(city){
+  
+   if (city === 'NYC') {
+    return 'Ok, sounds good.';
+   }
+   
+  else {
+    return'No go.';
+   }
+   
+ 
+  }
   // Write your code here!
-}
+
+
+function switchOnCharmFromTip(generous) {
+  if (generous === 'generous') {
+   return 'Thank you so much.';
+ }
+ else if (generous === 'not as generous'){
+  return 'Thank you.';
+ }
+ else {
+  return 'Bye.';
+ }
+ }
+  // Write your code here!
